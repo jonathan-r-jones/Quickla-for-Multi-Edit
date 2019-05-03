fd91ab059379069ac7e640b367d495e0d8c681a6
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2019-05-03 18:52:18 -0400

    May-3-2019_6:52_PM

--- Jonathan's_Macros.s
+++ Jonathan's_Macros.s
@@ -2065,7 +2065,7 @@ str fp = 'The number of windows to keep open, by default.';
 switch(@first_3_characters(@lower(get_environment("computername"))))
 {
   case "lip":
-    return(5);
+    return(7);
     break;
   default:
     return(12);
