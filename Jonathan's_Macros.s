d3af3d1d330f64163530a323551c9fe15f930805
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2020-06-29 10:23:12 -0400

    Jun-29-2020_10:23_AM

--- Jonathan's_Macros.s
+++ Jonathan's_Macros.s
@@ -4977,6 +4977,9 @@ url = @get_remote_oj_using_klc('rfreas', is_found);
 url = @get_remote_oj_using_klc('rfwsj', is_found);
 @surf(url, 1);
 
+url = @get_remote_oj_using_klc('tuca', is_found);
+@surf(url, 1);
+
 @footer;
 @say(fp);
 }
